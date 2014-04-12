6b99d988c077fae91fe191659c9efeeba26c2b77
Piotr Perak <piotrperak@poczta.onet.pl>
2014-04-12 23:38:04 +0200

    Updated package to work with newer versions of VS

--- Package/tools/install.ps1
+++ Package/tools/install.ps1
@@ -2,30 +2,30 @@ param($installPath, $toolsPath, $package, $project)
 
 $snippetFolder = "NUnit"
 $source = "$toolsPath\Snippets\*"
-# add 2005, 2008 below if You want. I haven't tested these snippets with those verions of VS 
-$vsVersions = @("2010")
-Foreach ($vsVersion in $vsVersions)
+
+$vsVersion = [System.Version]::Parse($dte.Version)
+$versionDir = "20" + ($vsVersion.Major + 1).ToString()
+$myCodeSnippetsFolder = "$HOME\Documents\Visual Studio $versionDir\Code Snippets\Visual C#\My Code Snippets\"
+
+if (Test-Path $myCodeSnippetsFolder)
 {
-	$myCodeSnippetsFolder = "$HOME\Documents\Visual Studio $vsVersion\Code Snippets\Visual C#\My Code Snippets\"
-	if (Test-Path $myCodeSnippetsFolder)
+	$destination = "$myCodeSnippetsFolder$snippetFolder"
+	if (!(Test-Path $destination))
 	{
-		$destination = "$myCodeSnippetsFolder$snippetFolder"
-		if (!(Test-Path $destination))
-		{
-		  New-Item $destination -itemType "directory"
-		}
-
-		write-host ========================================================================================================================================================================
-		write-host Copying snippets to $destination
-		write-host 
-
-		Copy-Item $source -Destination $destination -Recurse -Force
-
-		write-host Snippets are available for every project in every solution!
-		write-host Look at $destination\NUnit_snippets_summary.txt to find out what snippets are available
-		write-host
-		write-host To uninstall snippets just remove $destination directory
-		write-host ========================================================================================================================================================================
+		New-Item $destination -itemType "directory"
 	}
+
+	write-host ========================================================================================================================================================================
+	write-host Copying snippets to $destination
+	write-host 
+
+	Copy-Item $source -Destination $destination -Recurse -Force
+
+	write-host Snippets are available for every project in every solution!
+	write-host Look at $destination\NUnit_snippets_summary.txt to find out what snippets are available
+	write-host
+	write-host To uninstall snippets just remove $destination directory
+	write-host ========================================================================================================================================================================
 }
+
 uninstall-package NUnit.Snippets -ProjectName $project.Name
